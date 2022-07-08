d406cc2485539166e96d1efa010f278d8143c28b
ChoiDaeYoung <66999597+ChoiDaeYoung-94@users.noreply.github.com>
2022-07-08 13:58:00 +0900

    base scene 작업

---
+++ Tamer/Assets/02. Scripts/Managers/Sub/RuntimeInitialize.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RuntimeInitialize : MonoBehaviour
+{
+    [SerializeField] GameObject _go_console = null;
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void FirstLoad()
+    {
+#if UNITY_EDITOR
+        if (SceneManager.GetActiveScene().name.CompareTo("Login") != 0)
+            SceneManager.LoadScene("Login");
+#endif
+    }
+
+    private void Awake()
+    {
+#if DEVELOPMENT_BUILD
+        _go_console.SetActive(true);
+#endif
+    }
+}
