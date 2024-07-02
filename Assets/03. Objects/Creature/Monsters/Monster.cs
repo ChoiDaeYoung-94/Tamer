380bbbc38abe82ab860288d7dee4c2d4b406ad92
ChoiDaeYoung-94 <chleodud1410@gmail.com>
2024-07-02 21:48:33 +0900

    군집 이동 구현 #14

--- Assets/03. Objects/Creature/Monsters/Monster.cs
+++ Assets/03. Objects/Creature/Monsters/Monster.cs
@@ -1,3 +1,7 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,21 +12,23 @@ public class Monster : BaseController
     [Header("--- 세팅 ---")]
     [SerializeField] AD.Define.Monsters _monster;
     [SerializeField] NavMeshAgent _navAgent;
-    [SerializeField, Tooltip("통솔 오브젝트 - Monster 중 commander or player")] internal GameObject _go_commander = null;
-    [SerializeField, Tooltip("Monster 통솔 오브젝트 여부")] internal bool isCommander = false;
-    [SerializeField, Tooltip("통솔 오브젝트 멈충 상태")] internal bool isCommanderStop = false;
-    [SerializeField, Tooltip("통솔 오브젝트 위임 및 다른 monster 통제")] internal List<Monster> _list_groupMonsters = new List<Monster>();
-    [SerializeField, Tooltip("포획된 몬스터인지 여부")] bool isAlly = false;
+
+    [Header("--- 참고 ---")]
+    [SerializeField, Tooltip("통솔 오브젝트 - Monster 중 commander or player")] GameObject _go_commander = null;
+    [SerializeField, Tooltip("Commander Monster 여부")] bool isCommander = false;
+    [SerializeField, Tooltip("Commander Monster의 random 이동 최대 반경")] float moveRadius = 5.0f;
+    [SerializeField, Tooltip("통솔 오브젝트 위임 및 다른 monster 통제")] List<Monster> _list_groupMonsters = new List<Monster>();
+    [SerializeField, Tooltip("군집이동 반경 기본 2f, monster 크기에 따라 변경 됨")] float flockingRadius = 2f;
     [SerializeField, Tooltip("포획 가능한 몬스터인지 여부")] bool isAbleAlly = false;
+    [SerializeField, Tooltip("포획된 몬스터인지 여부")] bool isAlly = false;
+    [SerializeField, Tooltip("player 및 monster 감지를 위한 Coroutine")] Coroutine _co_detection = null;
     [SerializeField, Tooltip("player 및 monster 감지 범위")] float detectionRadius = 10.0f;
     [SerializeField, Tooltip("감지 여부")] bool isDetection = false;
-    [Tooltip("감지 layer")] int detectionLayer = 0;
-    [Tooltip("감지한 오브젝트의 position")] Vector3 _vec_detection = Vector3.zero;
-    [Tooltip("player 및 monster 감지를 위한 Coroutine")] Coroutine _co_detection = null;
-    [Tooltip("MonsgerCommander의 random 이동 최대 반경")] float moveRadius = 5.0f;
+    [SerializeField, Tooltip("감지 layer")] int detectionLayer = 0;
+    [SerializeField, Tooltip("감지한 오브젝트의 position")] Vector3 _vec_detection = Vector3.zero;
+    [SerializeField] private bool isDie = false;
     private float updateTime = 4f;
     private float updateTimer = 0f;
-    private bool isDie = false;
 
     [Header("--- 테스트 ---")]
     public Transform _player = null;
@@ -103,13 +109,6 @@ public class Monster : BaseController
                     CommanderMove();
                     updateTimer = 0;
                 }
-
-                CheckCommanderStatus();
-            }
-
-            if (!isCommander)
-            {
-                FollowerMove();
             }
         }
     }
@@ -137,48 +136,47 @@ public class Monster : BaseController
 
     private void CommanderMove()
     {
-        //if (Vector3.Distance(_player.position, transform.position) > 15f)
-        //{
-        //    _navAgent.SetDestination(_player.position);
-        //    return;
-        //}
+        Vector3 temp_randomDirection = Vector3.zero;
 
-        Vector3 temp_randomDirection = Random.insideUnitSphere * moveRadius;
-        temp_randomDirection += transform.position;
+        float distance = Vector3.Distance(_player.position, transform.position);
+
+        if (distance > 20f)
+        {
+            Vector3 directionToPlayer = (_player.position - transform.position).normalized;
+            temp_randomDirection = _player.position - directionToPlayer * 15f;
+        }
+        else
+        {
+            temp_randomDirection = Random.insideUnitSphere * moveRadius;
+            temp_randomDirection.y = 0f;
+            temp_randomDirection += transform.position;
+        }
 
         NavMeshHit hit;
         Vector3 finalPosition = Vector3.zero;
         if (NavMesh.SamplePosition(temp_randomDirection, out hit, 5f, 1))
             finalPosition = hit.position;
 
-        _navAgent.SetDestination(finalPosition);
+        int row = 1;
+        int countInRow = 0;
+        Vector3 startRowPosition = finalPosition;
 
-        foreach (Monster monster in _list_groupMonsters)
+        for (int i = -1; ++i < _list_groupMonsters.Count;)
         {
-            if (monster.gameObject != this.gameObject)
+            if (countInRow >= row)
             {
-                monster.isCommanderStop = false;
+                row++;
+                countInRow = 0;
+                startRowPosition += (Vector3.forward * flockingRadius * -1f);
             }
-        }
-    }
 
-    private void CheckCommanderStatus()
-    {
-        if (_navAgent.remainingDistance <= _navAgent.stoppingDistance && _navAgent.velocity.sqrMagnitude == 0f)
-        {
-            foreach (Monster monster in _list_groupMonsters)
-            {
-                if (monster.gameObject != this.gameObject)
-                {
-                    monster.isCommanderStop = true;
-                }
-            }
-        }
-    }
+            Vector3 positionOffset = _list_groupMonsters.Count - 1 == i ? Vector3.zero : (Vector3.right * (countInRow - (row - 1) / 2.0f) * flockingRadius);
+            Vector3 position = startRowPosition + positionOffset;
 
-    private void FollowerMove()
-    {
-        _navAgent.SetDestination(_go_commander.transform.position);
+            _list_groupMonsters[i]._navAgent.SetDestination(position);
+
+            countInRow++;
+        }
     }
 
     private void Die()
@@ -228,34 +226,31 @@ public class Monster : BaseController
 
     private void OnTriggerEnter(Collider col)
     {
-        if (!isCommander)
-        {
-            if (col.gameObject == _go_commander)
-                _navAgent.isStopped = true;
-        }
+
     }
 
     private void OnTriggerStay(Collider col)
     {
-        if (!isCommander)
-        {
-            if (isCommanderStop)
-            {
-                if (col.gameObject.layer == gameObject.layer && Vector3.Distance(transform.position, _go_commander.transform.position) < 3f)
-                {
-                    _navAgent.isStopped = true;
-                }
-            }
-            else
-                _navAgent.isStopped = false;
-        }
+
     }
 
     private void OnTriggerExit(Collider col)
     {
-        if (!isCommander && col.gameObject == _go_commander)
+
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(Monster))]
+    public class customEditor : Editor
+    {
+        public override void OnInspectorGUI()
         {
-            _navAgent.isStopped = false;
+            EditorGUILayout.HelpBox("Monster " +
+                "\n Layer를 통해 Ally, Enemy로 구분" +
+                "\n 군집이동은 commander monster를 통해 이동하며 최대 몬스터 객체 수는 4", MessageType.Info);
+
+            base.OnInspectorGUI();
         }
     }
+#endif
 }
