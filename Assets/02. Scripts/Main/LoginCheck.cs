d789c63e21f30ca7b117a583f01bcff40efea4f8
ChoiDaeYoung-94 <chleodud1410@gmail.com>
2025-02-10 10:29:40 +0900

    LoginCheck.cs 확장성, 코드 개선 #78

--- Assets/02. Scripts/Main/LoginCheck.cs
+++ Assets/02. Scripts/Main/LoginCheck.cs
@@ -1,14 +1,8 @@
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LoginCheck : MonoBehaviour
 {
-    private State _state;
+    private ILoginState _currentState;
 
     /// <summary>
     /// InitializeMain.cs 에서 호출
@@ -18,54 +12,36 @@ public class LoginCheck : MonoBehaviour
         if (!AD.Managers.GameM.LoginCheck)
             return;
 
-        _state = new SetCharacterState(this);
-        _state.Handle();
+        SetState(new SetCharacterState(this));
     }
 
-    public void SetState(State state)
+    public void SetState(ILoginState newState)
     {
-        _state = state;
-        _state.Handle();
+        _currentState = newState;
+        _currentState?.Handle();
     }
-
-#if UNITY_EDITOR
-    [CustomEditor(typeof(LoginCheck))]
-    public class customEditor : Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            EditorGUILayout.HelpBox("로그인시 해야 할 작업을 위한 데이터 체크" +
-                "\n1. 성별에 따른 캐릭터 생성" +
-                "\n2. Tutorial 여부", MessageType.Info);
-
-            base.OnInspectorGUI();
-        }
-    }
-#endif
 }
 
 #region LoginCheck Step State
-/// <summary>
-/// 여러 상태를 정의하기 위해 abstract 사용
-/// 아래 State를 상속 받는 class들의 순서대로 상태 진입
-/// </summary>
-public abstract class State
-{
-    protected LoginCheck _loginCheck;
 
-    public State(LoginCheck loginCheck)
+/// <summary>
+/// 여러 상태를 정의하기 위해 인터페이스 사용
+/// </summary>
+public interface ILoginState
+{
+    void Handle();
+}
+
+public class SetCharacterState : ILoginState
+{
+    private readonly LoginCheck _loginCheck;
+
+    public SetCharacterState(LoginCheck loginCheck)
     {
         _loginCheck = loginCheck;
     }
 
-    public abstract void Handle();
-}
-
-class SetCharacterState : State
-{
-    public SetCharacterState(LoginCheck loginCheck) : base(loginCheck) { }
-
-    public override void Handle()
+    public void Handle()
     {
         AD.DebugLogger.Log("LoginCheck", "SetCharacterState 진입");
 
@@ -79,11 +55,16 @@ class SetCharacterState : State
     }
 }
 
-class CheckTutorialState : State
+public class CheckTutorialState : ILoginState
 {
-    public CheckTutorialState(LoginCheck loginCheck) : base(loginCheck) { }
+    private readonly LoginCheck _loginCheck;
 
-    public override void Handle()
+    public CheckTutorialState(LoginCheck loginCheck)
+    {
+        _loginCheck = loginCheck;
+    }
+
+    public void Handle()
     {
         AD.DebugLogger.Log("LoginCheck", "CheckTutorialState 진입");
 
@@ -93,4 +74,5 @@ class CheckTutorialState : State
         }
     }
 }
+
 #endregion
