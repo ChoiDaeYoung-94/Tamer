e25eb1aaaf9704bb86fede33ddab635a995ee4bc
ChoiDaeYoung-94 <chleodud1410@gmail.com>
2025-02-10 16:18:24 +0900

    PlayerUICanvas.cs 코드 개선 #78

--- Assets/03. Objects/MainAndGameUICanvas/PlayerUICanvas.cs
+++ Assets/03. Objects/MainAndGameUICanvas/PlayerUICanvas.cs
@@ -1,9 +1,3 @@
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,43 +5,42 @@ using TMPro;
 
 public class PlayerUICanvas : MonoBehaviour
 {
-    static PlayerUICanvas instance;
-    public static PlayerUICanvas Instance { get { return instance; } }
+    private static PlayerUICanvas _instance;
+    public static PlayerUICanvas Instance { get { return _instance; } }
 
     [Header("--- 세팅 panel_playerInfo ---")]
-    [SerializeField] TMP_Text _TMP_playerNickName = null;
-    [SerializeField] TMP_Text _TMP_captureCapacity = null;
-    [SerializeField] TMP_Text _TMP_gold = null;
-    [SerializeField] TMP_Text _TMP_HP = null;
-    [SerializeField, Tooltip("Player HP UI")] Slider _Slider_HP = null;
+    [SerializeField] private TMP_Text _playerNickNameText;
+    [SerializeField] private TMP_Text _captureCapacityText;
+    [SerializeField] private TMP_Text _goldText;
+    [SerializeField] private TMP_Text _playerHpText;
+    [SerializeField] private Slider _playerHpSlider;
 
     [Header("--- 세팅 In Popup_playerInfo ---")]
-    [SerializeField, Tooltip("상단 패널 클릭 시 status popup")] GameObject _go_Popup_playerInfo = null;
-    [SerializeField] TMP_Text _TMP_POPplayerNickName = null;
-    [SerializeField] TMP_Text _TMP_POPcaptureCapacity = null;
-    [SerializeField] TMP_Text _TMP_POPgold = null;
-    [SerializeField] TMP_Text _TMP_POPpower = null;
-    [SerializeField] TMP_Text _TMP_POPattackSpeed = null;
-    [SerializeField] TMP_Text _TMP_POPmoveSpeed = null;
+    [SerializeField] private GameObject _popupPlayerInfoPanel;
+    [SerializeField] private TMP_Text _popupNickNameText;
+    [SerializeField] private TMP_Text _popupCaptureCapacityText;
+    [SerializeField] private TMP_Text _popupGoldText;
+    [SerializeField] private TMP_Text _popupPowerText;
+    [SerializeField] private TMP_Text _popupAttackSpeedText;
+    [SerializeField] private TMP_Text _popupMoveSpeedText;
 
     [Header("--- 세팅 panel_playerBuff ---")]
-    [SerializeField, Tooltip("AdMobBuff 남은 시간 표기하는 panel")] GameObject _go_panelAdMobBuff = null;
-    [SerializeField, Tooltip("AdMobBuff 남은 시간 표기하는 TMP")] TMP_Text _TMP_remainingBuffTime = null;
+    [SerializeField] private GameObject _buffPanel;
+    [SerializeField] private TMP_Text _buffTimeText;
 
     [Header("--- ETC ---")]
-    [SerializeField, Tooltip("Game scene 진입 시")] GameObject _go_panel_gamesceneUI = null;
-    [SerializeField, Tooltip("monster를 포획 가능 할 경우 포획 버튼 활성화")] GameObject _go_capture = null;
+    [SerializeField] private GameObject _gameSceneUIPanel;
+    [SerializeField] private GameObject _captureButton;
 
-    [Header("--- 참고용 ---")]
-    private bool _isBuff = false;
-    private double _remainBuffTime = 0f;
+    private bool _isBuffActive = false;
+    private double _remainingBuffTime = 0f;
 
     /// <summary>
     /// LoginCheck.cs 에서 생성
     /// </summary>
     private void Awake()
     {
-        instance = this;
+        _instance = this;
         DontDestroyOnLoad(gameObject);
     }
 
@@ -66,14 +59,15 @@ public class PlayerUICanvas : MonoBehaviour
     #region Functions
 
     #region Data Setting
+
     /// <summary>
     /// 뷰 세팅
     /// Main scene 진입 시, Main Game scene 전환 시
     /// </summary>
     public void ViewSettings()
     {
-        _go_Popup_playerInfo.SetActive(false);
-        _go_panel_gamesceneUI.SetActive(AD.Managers.GameM.IsGame);
+        _popupPlayerInfoPanel.SetActive(false);
+        _gameSceneUIPanel.SetActive(AD.Managers.GameM.IsGame);
     }
 
     /// <summary>
@@ -81,8 +75,7 @@ public class PlayerUICanvas : MonoBehaviour
     /// </summary>
     private void DataSettings()
     {
-        _TMP_playerNickName.text = $"{AD.Managers.DataM.LocalPlayerData["NickName"]}";
-
+        _playerNickNameText.text = $"{AD.Managers.DataM.LocalPlayerData["NickName"]}";
         UpdatePlayerInfo();
         UpdatePopPlayerInfo();
     }
@@ -92,14 +85,14 @@ public class PlayerUICanvas : MonoBehaviour
     /// </summary>
     public void UpdatePlayerInfo()
     {
-        _TMP_captureCapacity.text = $"{Player.Instance.GetCurMonsterCount()} / {Player.Instance._maxCaptureCapacity}";
-        _TMP_gold.text = $"Gold - {Player.Instance.Gold}";
+        _captureCapacityText.text = $"{Player.Instance.GetCurMonsterCount()} / {Player.Instance._maxCaptureCapacity}";
+        _goldText.text = $"Gold - {Player.Instance.Gold}";
 
         float maxHP = Player.Instance.ItemHp > Player.Instance.OrgHp ? Player.Instance.ItemHp : Player.Instance.OrgHp;
-        _TMP_HP.text = $"{Player.Instance.Hp} / {maxHP}";
+        _playerHpText.text = $"{Player.Instance.Hp} / {maxHP}";
 
-        _Slider_HP.maxValue = maxHP;
-        _Slider_HP.value = Player.Instance.Hp;
+        _playerHpSlider.maxValue = maxHP;
+        _playerHpSlider.value = Player.Instance.Hp;
     }
 
     /// <summary>
@@ -107,14 +100,20 @@ public class PlayerUICanvas : MonoBehaviour
     /// </summary>
     private void UpdatePopPlayerInfo()
     {
-        _TMP_POPplayerNickName.text = $"NickName - {AD.Managers.DataM.LocalPlayerData["NickName"]}";
-        _TMP_POPcaptureCapacity.text = $"CaptureCapacity - {Player.Instance.GetCurMonsterCount()} / {Player.Instance._maxCaptureCapacity}";
-        _TMP_POPgold.text = $"Gold - {Player.Instance.Gold}";
+        _popupNickNameText.text = $"NickName - {AD.Managers.DataM.LocalPlayerData["NickName"]}";
+        _popupCaptureCapacityText.text = $"CaptureCapacity - {Player.Instance.GetCurMonsterCount()} / {Player.Instance._maxCaptureCapacity}";
+        _popupGoldText.text = $"Gold - {Player.Instance.Gold}";
 
-        _TMP_POPpower.text = _isBuff ? $"Power - {Player.Instance._buffPower}" : $"Power - {Player.Instance.Power}";
-        _TMP_POPattackSpeed.text = _isBuff ? $"AttackSpeed - {Player.Instance._buffAttackSpeed}" : $"AttackSpeed - {Player.Instance.AttackSpeed}";
-        _TMP_POPmoveSpeed.text = _isBuff ? $"MoveSpeed - {Player.Instance._buffMoveSpeed}" : $"MoveSpeed - {Player.Instance.MoveSpeed}";
+        _popupPowerText.text = GetPlayerStat(Player.Instance.Power, Player.Instance._buffPower, "Power");
+        _popupAttackSpeedText.text = GetPlayerStat(Player.Instance.AttackSpeed, Player.Instance._buffAttackSpeed, "AttackSpeed");
+        _popupMoveSpeedText.text = GetPlayerStat(Player.Instance.MoveSpeed, Player.Instance._buffMoveSpeed, "MoveSpeed");
     }
+
+    private string GetPlayerStat(float normalStat, float buffStat, string stat)
+    {
+        return _isBuffActive ? $"{stat} - {buffStat}" : $"{stat} - {normalStat}";
+    }
+
     #endregion
 
     /// <summary>
@@ -127,7 +126,7 @@ public class PlayerUICanvas : MonoBehaviour
         UpdatePopPlayerInfo();
 
         Time.timeScale = 0;
-        _go_Popup_playerInfo.SetActive(true);
+        _popupPlayerInfoPanel.SetActive(true);
     }
 
     public void OpenSetting()
@@ -160,18 +159,19 @@ public class PlayerUICanvas : MonoBehaviour
     public void ClosePopupSetting() => Time.timeScale = 1;
 
     #region GoogleAdMob Buff
+
     /// <summary>
     /// BuffingMan.cs을 통해 변동되는 data로 작동
     /// buffpanel 관리 위함
     /// </summary>
     private void UpdateBuffPanel()
     {
-        if (_isBuff)
+        if (_isBuffActive)
         {
-            _TMP_remainingBuffTime.text = AD.TimeUtility.FormatTimeString(_remainBuffTime, padZero: true, includeSeconds: true, useColon: true);
+            _buffTimeText.text = AD.TimeUtility.FormatTimeString(_remainingBuffTime, padZero: true, includeSeconds: true, useColon: true);
 
-            _remainBuffTime -= Time.deltaTime;
-            if (_remainBuffTime <= 0f)
+            _remainingBuffTime -= Time.deltaTime;
+            if (_remainingBuffTime <= 0f)
                 AD.Managers.GoogleAdMobM.ResetAdMob();
         }
     }
@@ -181,12 +181,11 @@ public class PlayerUICanvas : MonoBehaviour
     /// </summary>
     public void SetBuff(double remainTime)
     {
-        _remainBuffTime = remainTime;
-        _isBuff = true;
+        _remainingBuffTime = remainTime;
+        _isBuffActive = true;
 
         UpdatePopPlayerInfo();
-
-        _go_panelAdMobBuff.SetActive(true);
+        _buffPanel.SetActive(true);
     }
 
     /// <summary>
@@ -194,38 +193,24 @@ public class PlayerUICanvas : MonoBehaviour
     /// </summary>
     public void EndBuff()
     {
-        _go_panelAdMobBuff.SetActive(false);
-
-        _isBuff = false;
-
+        _buffPanel.SetActive(false);
+        _isBuffActive = false;
         UpdatePopPlayerInfo();
     }
+
     #endregion
 
-    public void EnableCapture() => _go_capture.SetActive(true);
+    public void EnableCapture() => _captureButton.SetActive(true);
 
-    public void DisableCapture() => _go_capture.SetActive(false);
+    public void DisableCapture() => _captureButton.SetActive(false);
 
     public void OnClickCapture()
     {
         AD.Managers.SoundM.UI_Click();
 
         Player.Instance.Capture();
-        _go_capture.SetActive(false);
+        _captureButton.SetActive(false);
     }
 
     #endregion
-
-#if UNITY_EDITOR
-    [CustomEditor(typeof(PlayerUICanvas))]
-    public class customEditor : Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            EditorGUILayout.HelpBox("Main, Game scene에서 Player에 대한 정보, 세팅, minimap등 UI 제공", MessageType.Info);
-
-            base.OnInspectorGUI();
-        }
-    }
-#endif
 }
